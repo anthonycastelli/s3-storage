992cac0f6443cfa28664f17250e4ff777fb287cf
Anthony Castelli <neueanthony@me.com>
2018-04-23 13:21:22 -0700

    tests: Finish renaming tests

--- Tests/S3StorageTests/XCTestManifests.swift
+++ Tests/S3StorageTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(StorageS3Tests.allTests),
+        testCase(S3StorageTests.allTests),
     ]
 }
 #endif
