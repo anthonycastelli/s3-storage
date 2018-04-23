cd133c3809d8e9a2e10827e4059d746f85c88ee3
Anthony Castelli <neueanthony@me.com>
2018-04-23 13:06:07 -0700

    tests: Rename tests

--- Tests/S3StorageTests/S3StorageTests.swift
+++ Tests/S3StorageTests/S3StorageTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import StorageS3
+@testable import S3Storage
 
-final class StorageS3Tests: XCTestCase {
+final class S3StorageTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
